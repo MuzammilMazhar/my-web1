daae2931b84ab55c6a7ff9ff5402b0f9c42a7173
Muzammil-Mazhar <140798237+Muzammil-Mazhar@users.noreply.github.com>
2024-05-14 18:56:56 +0100

    new commit

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,16 +4,18 @@ import React from "react";
 function Product(){
     return(
         <div className="mt-24 grid grid-cols-1 p-20">
-            <div>
-                <h1 className="text-9xl font-bold">Design</h1>
-            </div>
-            <div className="justify-items-center justify-self-center">
-                <h1 className="text-9xl font-bold ">For</h1>
-            </div>
-            <div>
-                <h1 className="text-9xl font-bold float-right">Demo</h1>
-            </div>
+        <h1  className="text-4xl sm:text-6xl md:text-8xl lg:text-9xl font-bold">Product Page</h1>
+        <div>
+            <h1  className="text-4xl sm:text-6xl md:text-8xl lg:text-9xl font-bold">Design</h1>
         </div>
+        <div className="justify-items-center justify-self-center">
+            <h1  className="text-4xl sm:text-6xl md:text-8xl lg:text-9xl font-bold">For</h1>
+        </div>
+        <div>
+            <h1  className="text-4xl sm:text-6xl md:text-8xl lg:text-9xl font-bold float-right">Demo</h1>
+        </div>
+
+    </div>
     )
 }
 
